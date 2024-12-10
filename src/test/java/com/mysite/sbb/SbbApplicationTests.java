4beca43f7337d37671ed15ce208b000728749e59
sungyeong98 <am25227903@gmail.com>
2024-12-10 12:25:10 +0900

    refactor: 클래스 위치 변경

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -2,12 +2,15 @@ package com.mysite.sbb;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
