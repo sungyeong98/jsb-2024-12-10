ab6e56bc612ef030630e8b441b754fad77b4ea1e
sungyeong98 <am25227903@gmail.com>
2024-12-11 12:30:27 +0900

    refactor: 코드 수정

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -39,13 +39,16 @@ public class AnswerController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
-    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult, @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
         Answer answer = this.answerService.getAnswer(id);
         if (!answer.getAuthor().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
-        answerForm.setContent(answer.getContent());
-        return "answer_form";
+        this.answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/answer/detail/%s", answer.getQuestion().getId());
     }
 
 }
